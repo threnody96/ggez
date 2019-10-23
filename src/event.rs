42177e900239f08edb52591e3afe9f75c856804f
Takashi, Yano <t.yano@mmj.ne.jp>
2019-10-23 22:11:39 +0900

    0.5

--- src/event.rs
+++ src/event.rs
@@ -55,19 +55,18 @@ use crate::error::GameResult;
 pub trait EventHandler {
     /// Called upon each logic update to the game.
     /// This should be where the game's logic takes place.
-    fn update(&mut self, _ctx: &mut Context) -> GameResult;
+    fn update(&mut self) -> GameResult;
 
     /// Called to do the drawing of your game.
     /// You probably want to start this with
     /// [`graphics::clear()`](../graphics/fn.clear.html) and end it
     /// with [`graphics::present()`](../graphics/fn.present.html) and
     /// maybe [`timer::yield_now()`](../timer/fn.yield_now.html).
-    fn draw(&mut self, _ctx: &mut Context) -> GameResult;
+    fn draw(&mut self) -> GameResult;
 
     /// A mouse button was pressed
     fn mouse_button_down_event(
         &mut self,
-        _ctx: &mut Context,
         _button: MouseButton,
         _x: f32,
         _y: f32,
@@ -77,7 +76,6 @@ pub trait EventHandler {
     /// A mouse button was released
     fn mouse_button_up_event(
         &mut self,
-        _ctx: &mut Context,
         _button: MouseButton,
         _x: f32,
         _y: f32,
@@ -86,11 +84,11 @@ pub trait EventHandler {
 
     /// The mouse was moved; it provides both absolute x and y coordinates in the window,
     /// and relative x and y coordinates compared to its last position.
-    fn mouse_motion_event(&mut self, _ctx: &mut Context, _x: f32, _y: f32, _dx: f32, _dy: f32) {}
+    fn mouse_motion_event(&mut self, _x: f32, _y: f32, _dx: f32, _dy: f32) {}
 
     /// The mousewheel was scrolled, vertically (y, positive away from and negative toward the user)
     /// or horizontally (x, positive to the right and negative to the left).
-    fn mouse_wheel_event(&mut self, _ctx: &mut Context, _x: f32, _y: f32) {}
+    fn mouse_wheel_event(&mut self, _x: f32, _y: f32) {}
 
     /// A keyboard button was pressed.
     ///
@@ -100,186 +98,181 @@ pub trait EventHandler {
     /// functionality yourself.
     fn key_down_event(
         &mut self,
-        ctx: &mut Context,
         keycode: KeyCode,
         _keymods: KeyMods,
         _repeat: bool,
-    ) {
-        if keycode == KeyCode::Escape {
-            quit(ctx);
-        }
-    }
+    ) { }
 
     /// A keyboard button was released.
-    fn key_up_event(&mut self, _ctx: &mut Context, _keycode: KeyCode, _keymods: KeyMods) {}
+    fn key_up_event(&mut self, _keycode: KeyCode, _keymods: KeyMods) {}
 
     /// A unicode character was received, usually from keyboard input.
     /// This is the intended way of facilitating text input.
-    fn text_input_event(&mut self, _ctx: &mut Context, _character: char) {}
+    fn text_input_event(&mut self, _character: char) {}
 
     /// A gamepad button was pressed; `id` identifies which gamepad.
     /// Use [`input::gamepad()`](../input/fn.gamepad.html) to get more info about
     /// the gamepad.
-    fn gamepad_button_down_event(&mut self, _ctx: &mut Context, _btn: Button, _id: GamepadId) {}
+    fn gamepad_button_down_event(&mut self, _btn: Button, _id: GamepadId) {}
 
     /// A gamepad button was released; `id` identifies which gamepad.
     /// Use [`input::gamepad()`](../input/fn.gamepad.html) to get more info about
     /// the gamepad.
-    fn gamepad_button_up_event(&mut self, _ctx: &mut Context, _btn: Button, _id: GamepadId) {}
+    fn gamepad_button_up_event(&mut self, _btn: Button, _id: GamepadId) {}
 
     /// A gamepad axis moved; `id` identifies which gamepad.
     /// Use [`input::gamepad()`](../input/fn.gamepad.html) to get more info about
     /// the gamepad.
-    fn gamepad_axis_event(&mut self, _ctx: &mut Context, _axis: Axis, _value: f32, _id: GamepadId) {
+    fn gamepad_axis_event(&mut self, _axis: Axis, _value: f32, _id: GamepadId) {
     }
 
     /// Called when the window is shown or hidden.
-    fn focus_event(&mut self, _ctx: &mut Context, _gained: bool) {}
+    fn focus_event(&mut self, _gained: bool) {}
 
     /// Called upon a quit event.  If it returns true,
     /// the game does not exit (the quit event is cancelled).
-    fn quit_event(&mut self, _ctx: &mut Context) -> bool {
+    fn quit_event(&mut self) -> bool {
         debug!("quit_event() callback called, quitting...");
         false
     }
 
     /// Called when the user resizes the window, or when it is resized
     /// via [`graphics::set_mode()`](../graphics/fn.set_mode.html).
-    fn resize_event(&mut self, _ctx: &mut Context, _width: f32, _height: f32) {}
+    fn resize_event(&mut self, _width: f32, _height: f32) {}
 }
 
-/// Terminates the [`ggez::event::run()`](fn.run.html) loop by setting
-/// [`Context.continuing`](struct.Context.html#structfield.continuing)
-/// to `false`.
-pub fn quit(ctx: &mut Context) {
-    ctx.continuing = false;
-}
+// Terminates the [`ggez::event::run()`](fn.run.html) loop by setting
+// [`Context.continuing`](struct.Context.html#structfield.continuing)
+// to `false`.
+// pub fn quit(ctx: &mut Context) {
+//     ctx.continuing = false;
+// }
 
-/// Runs the game's main loop, calling event callbacks on the given state
-/// object as events occur.
-///
-/// It does not try to do any type of framerate limiting.  See the
-/// documentation for the [`timer`](../timer/index.html) module for more info.
-pub fn run<S>(ctx: &mut Context, events_loop: &mut EventsLoop, state: &mut S) -> GameResult
-where
-    S: EventHandler,
-{
-    use crate::input::{keyboard, mouse};
-
-    while ctx.continuing {
-        // If you are writing your own event loop, make sure
-        // you include `timer_context.tick()` and
-        // `ctx.process_event()` calls.  These update ggez's
-        // internal state however necessary.
-        ctx.timer_context.tick();
-        events_loop.poll_events(|event| {
-            ctx.process_event(&event);
-            match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::Resized(logical_size) => {
-                        // let actual_size = logical_size;
-                        state.resize_event(
-                            ctx,
-                            logical_size.width as f32,
-                            logical_size.height as f32,
-                        );
-                    }
-                    WindowEvent::CloseRequested => {
-                        if !state.quit_event(ctx) {
-                            quit(ctx);
-                        }
-                    }
-                    WindowEvent::Focused(gained) => {
-                        state.focus_event(ctx, gained);
-                    }
-                    WindowEvent::ReceivedCharacter(ch) => {
-                        state.text_input_event(ctx, ch);
-                    }
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(keycode),
-                                modifiers,
-                                ..
-                            },
-                        ..
-                    } => {
-                        let repeat = keyboard::is_key_repeated(ctx);
-                        state.key_down_event(ctx, keycode, modifiers.into(), repeat);
-                    }
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Released,
-                                virtual_keycode: Some(keycode),
-                                modifiers,
-                                ..
-                            },
-                        ..
-                    } => {
-                        state.key_up_event(ctx, keycode, modifiers.into());
-                    }
-                    WindowEvent::MouseWheel { delta, .. } => {
-                        let (x, y) = match delta {
-                            MouseScrollDelta::LineDelta(x, y) => (x, y),
-                            MouseScrollDelta::PixelDelta(dpi::LogicalPosition { x, y }) => {
-                                (x as f32, y as f32)
-                            }
-                        };
-                        state.mouse_wheel_event(ctx, x, y);
-                    }
-                    WindowEvent::MouseInput {
-                        state: element_state,
-                        button,
-                        ..
-                    } => {
-                        let position = mouse::position(ctx);
-                        match element_state {
-                            ElementState::Pressed => {
-                                state.mouse_button_down_event(ctx, button, position.x, position.y)
-                            }
-                            ElementState::Released => {
-                                state.mouse_button_up_event(ctx, button, position.x, position.y)
-                            }
-                        }
-                    }
-                    WindowEvent::CursorMoved { .. } => {
-                        let position = mouse::position(ctx);
-                        let delta = mouse::delta(ctx);
-                        state.mouse_motion_event(ctx, position.x, position.y, delta.x, delta.y);
-                    }
-                    _x => {
-                        // trace!("ignoring window event {:?}", x);
-                    }
-                },
-                Event::DeviceEvent { event, .. } => match event {
-                    _ => (),
-                },
-                Event::Awakened => (),
-                Event::Suspended(_) => (),
-            }
-        });
-        // Handle gamepad events if necessary.
-        if ctx.conf.modules.gamepad {
-            while let Some(gilrs::Event { id, event, .. }) = ctx.gamepad_context.next_event() {
-                match event {
-                    gilrs::EventType::ButtonPressed(button, _) => {
-                        state.gamepad_button_down_event(ctx, button, GamepadId(id));
-                    }
-                    gilrs::EventType::ButtonReleased(button, _) => {
-                        state.gamepad_button_up_event(ctx, button, GamepadId(id));
-                    }
-                    gilrs::EventType::AxisChanged(axis, value, _) => {
-                        state.gamepad_axis_event(ctx, axis, value, GamepadId(id));
-                    }
-                    _ => {}
-                }
-            }
-        }
-        state.update(ctx)?;
-        state.draw(ctx)?;
-    }
-
-    Ok(())
-}
+// Runs the game's main loop, calling event callbacks on the given state
+// object as events occur.
+//
+// It does not try to do any type of framerate limiting.  See the
+// documentation for the [`timer`](../timer/index.html) module for more info.
+// pub fn run<S>(ctx: &mut Context, events_loop: &mut EventsLoop, state: &mut S) -> GameResult
+// where
+//     S: EventHandler,
+// {
+//     use crate::input::{keyboard, mouse};
+// 
+//     while ctx.continuing {
+//         // If you are writing your own event loop, make sure
+//         // you include `timer_context.tick()` and
+//         // `ctx.process_event()` calls.  These update ggez's
+//         // internal state however necessary.
+//         ctx.timer_context.tick();
+//         events_loop.poll_events(|event| {
+//             ctx.process_event(&event);
+//             match event {
+//                 Event::WindowEvent { event, .. } => match event {
+//                     WindowEvent::Resized(logical_size) => {
+//                         // let actual_size = logical_size;
+//                         state.resize_event(
+//                             ctx,
+//                             logical_size.width as f32,
+//                             logical_size.height as f32,
+//                         );
+//                     }
+//                     WindowEvent::CloseRequested => {
+//                         if !state.quit_event(ctx) {
+//                             quit(ctx);
+//                         }
+//                     }
+//                     WindowEvent::Focused(gained) => {
+//                         state.focus_event(ctx, gained);
+//                     }
+//                     WindowEvent::ReceivedCharacter(ch) => {
+//                         state.text_input_event(ctx, ch);
+//                     }
+//                     WindowEvent::KeyboardInput {
+//                         input:
+//                             KeyboardInput {
+//                                 state: ElementState::Pressed,
+//                                 virtual_keycode: Some(keycode),
+//                                 modifiers,
+//                                 ..
+//                             },
+//                         ..
+//                     } => {
+//                         let repeat = keyboard::is_key_repeated(ctx);
+//                         state.key_down_event(ctx, keycode, modifiers.into(), repeat);
+//                     }
+//                     WindowEvent::KeyboardInput {
+//                         input:
+//                             KeyboardInput {
+//                                 state: ElementState::Released,
+//                                 virtual_keycode: Some(keycode),
+//                                 modifiers,
+//                                 ..
+//                             },
+//                         ..
+//                     } => {
+//                         state.key_up_event(ctx, keycode, modifiers.into());
+//                     }
+//                     WindowEvent::MouseWheel { delta, .. } => {
+//                         let (x, y) = match delta {
+//                             MouseScrollDelta::LineDelta(x, y) => (x, y),
+//                             MouseScrollDelta::PixelDelta(dpi::LogicalPosition { x, y }) => {
+//                                 (x as f32, y as f32)
+//                             }
+//                         };
+//                         state.mouse_wheel_event(ctx, x, y);
+//                     }
+//                     WindowEvent::MouseInput {
+//                         state: element_state,
+//                         button,
+//                         ..
+//                     } => {
+//                         let position = mouse::position(ctx);
+//                         match element_state {
+//                             ElementState::Pressed => {
+//                                 state.mouse_button_down_event(ctx, button, position.x, position.y)
+//                             }
+//                             ElementState::Released => {
+//                                 state.mouse_button_up_event(ctx, button, position.x, position.y)
+//                             }
+//                         }
+//                     }
+//                     WindowEvent::CursorMoved { .. } => {
+//                         let position = mouse::position(ctx);
+//                         let delta = mouse::delta(ctx);
+//                         state.mouse_motion_event(ctx, position.x, position.y, delta.x, delta.y);
+//                     }
+//                     _x => {
+//                         // trace!("ignoring window event {:?}", x);
+//                     }
+//                 },
+//                 Event::DeviceEvent { event, .. } => match event {
+//                     _ => (),
+//                 },
+//                 Event::Awakened => (),
+//                 Event::Suspended(_) => (),
+//             }
+//         });
+//         // Handle gamepad events if necessary.
+//         if ctx.conf.modules.gamepad {
+//             while let Some(gilrs::Event { id, event, .. }) = ctx.gamepad_context.next_event() {
+//                 match event {
+//                     gilrs::EventType::ButtonPressed(button, _) => {
+//                         state.gamepad_button_down_event(ctx, button, GamepadId(id));
+//                     }
+//                     gilrs::EventType::ButtonReleased(button, _) => {
+//                         state.gamepad_button_up_event(ctx, button, GamepadId(id));
+//                     }
+//                     gilrs::EventType::AxisChanged(axis, value, _) => {
+//                         state.gamepad_axis_event(ctx, axis, value, GamepadId(id));
+//                     }
+//                     _ => {}
+//                 }
+//             }
+//         }
+//         state.update(ctx)?;
+//         state.draw(ctx)?;
+//     }
+// 
+//     Ok(())
+// }
